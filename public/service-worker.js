e2314be82b5e959c664bd50918c9e81feae04f0a
Rachel Sakles <rachel.sakles@yahoo.com>
2020-10-07 13:14:19 -0700

    added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,8 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+    "/icons/icon-512x512.png", 
+    "/db.js"
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
